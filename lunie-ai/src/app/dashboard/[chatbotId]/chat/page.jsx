88e7eb347b126472ebda339dd66234293865caf1
Muhammad Saad Shahid <fa23-bce-068@cuilahore.edu.pk>
2025-08-31 00:04:39 +0500

    feat: Update ChatPage to use dynamic chatbotId from URL parameters
    feat: Update ChatPage to use dynamic chatbotId from URL parameters

--- lunie-ai/src/app/dashboard/[chatbotId]/chat/page.jsx
+++ lunie-ai/src/app/dashboard/[chatbotId]/chat/page.jsx
@@ -1,9 +1,18 @@
 // import LoginForm from '@/components/auth/LoginForm'
+'use client'
+
+import React from 'react'
 import ChatInterface from '@/components/chat/ChatInterface'
+import { useParams } from 'next/navigation'
+
 
 export default function ChatPage() {
+  
+  const params = useParams()
+  const chatbotId = params?.chatbotId || '123'
+
   return <ChatInterface 
-  chatbotId="9604f284-2cd9-4d4b-9857-a7b16991af1c"  // ← Make sure this is set
+  chatbotId={chatbotId}  // ← Make sure this is set
   chatbot={{ name: "My Assistant" }}
 />
 }
